c51a108f383eb5ab978b63191e057b3af39842c7
surjanthakur <tsurjan506@gmail.com>
2025-10-03 00:23:36 +0530

    first commit

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,20 @@
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+database_url = os.getenv("DB_URL")
+
+if database_url:
+    engine = create_engine(database_url)
+
+
+# creating table in databse
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
 from router import listings, users
+from database import create_db
 
 app = FastAPI()
+
+
+# create database
+@app.on_event("startup")
+def on_startup():
+    try:
+        create_db()
+        print("db is connected ✅")
+    except Exception as err:
+        print(f"cannot connect to database: {err}")
